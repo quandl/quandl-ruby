84fc4faf8b3eea04954d0ff8a597aca8a014cd43
Clement Leung <clement@quandl.com>
2015-07-15 13:00:45 -0400

    thread safe

--- lib/quandl/config.rb
+++ lib/quandl/config.rb
@@ -1,8 +1,33 @@
 module Quandl
   class Config
-    @api_base = 'https://www.quandl.com/api/v3'
+    API_KEY_THREAD_KEY = 'quandl_api_key'
+    API_BASE_THREAD_KEY = 'quandl_api_base'
+    API_VERSION_THREAD_KEY = 'quandl_api_version_key'
+
     class << self
-      attr_accessor :api_key, :api_base, :api_version
+      def api_key=(api_key)
+        Thread.current[API_KEY_THREAD_KEY] = api_key
+      end
+
+      def api_key
+        Thread.current[API_KEY_THREAD_KEY]
+      end
+
+      def api_base=(api_base)
+        Thread.current[API_BASE_THREAD_KEY] = api_base
+      end
+
+      def api_base
+        Thread.current[API_BASE_THREAD_KEY] || 'https://www.quandl.com/api/v3'
+      end
+
+      def api_version=(api_version)
+        Thread.current[API_VERSION_THREAD_KEY] = api_version
+      end
+
+      def api_version
+        Thread.current[API_VERSION_THREAD_KEY]
+      end
     end
   end
 end
