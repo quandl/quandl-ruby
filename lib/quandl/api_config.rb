d74a0f5caf499ddfa36e5f6e9c66ef39b273efa1
Matthew Basset <matthew@quandl.com>
2015-09-08 14:39:27 -0400

    Allow for config defaults when working in a forked/threaded environment.

--- lib/quandl/api_config.rb
+++ lib/quandl/api_config.rb
@@ -4,29 +4,42 @@ module Quandl
     API_BASE_THREAD_KEY = 'quandl_api_base'
     API_VERSION_THREAD_KEY = 'quandl_api_version_key'
 
+    @api_key = nil
+    @api_base = nil
+    @api_version = nil
+
     class << self
       def api_key=(api_key)
+        @api_key = api_key if @api_key.nil?
         Thread.current[API_KEY_THREAD_KEY] = api_key
       end
 
       def api_key
-        Thread.current[API_KEY_THREAD_KEY]
+        Thread.current[API_KEY_THREAD_KEY] || @api_key
       end
 
       def api_base=(api_base)
+        @api_base = api_base if @api_base.nil?
         Thread.current[API_BASE_THREAD_KEY] = api_base
       end
 
       def api_base
-        Thread.current[API_BASE_THREAD_KEY] || 'https://www.quandl.com/api/v3'
+        Thread.current[API_BASE_THREAD_KEY] || @api_base || 'https://www.quandl.com/api/v3'
       end
 
       def api_version=(api_version)
+        @api_version = api_base if @api_version.nil?
         Thread.current[API_VERSION_THREAD_KEY] = api_version
       end
 
       def api_version
-        Thread.current[API_VERSION_THREAD_KEY]
+        Thread.current[API_VERSION_THREAD_KEY] || @api_version
+      end
+
+      def reset
+        @api_key = nil
+        @api_base = nil
+        @api_version = nil
       end
     end
   end
