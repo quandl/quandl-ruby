a368fa51d1aeb76b274c8f73030fe315af2d4e15
Jearvon Dharrie <j.dharrie@gmail.com>
2018-03-20 23:44:02 -0400

    Use ||= for initializing variables

--- lib/quandl/api_config.rb
+++ lib/quandl/api_config.rb
@@ -12,7 +12,7 @@ module Quandl
 
     class << self
       def api_key=(api_key)
-        @api_key = api_key if @api_key.nil?
+        @api_key ||= api_key
         Thread.current[API_KEY_THREAD_KEY] = api_key
       end
 
@@ -21,7 +21,7 @@ module Quandl
       end
 
       def api_base=(api_base)
-        @api_base = api_base if @api_base.nil?
+        @api_base ||= api_base
         Thread.current[API_BASE_THREAD_KEY] = api_base
       end
 
@@ -30,7 +30,7 @@ module Quandl
       end
 
       def api_version=(api_version)
-        @api_version = api_base if @api_version.nil?
+        @api_version ||= api_version
         Thread.current[API_VERSION_THREAD_KEY] = api_version
       end
 
